d5d08854fd3ab5c83489484ed2cfe5ec17af504e
Guoda Arlauskaite <guoda.arlauskaite@gmail.com>
2023-02-28 23:51:00 +0200

    created apply form

--- src/types/user.ts
+++ src/types/user.ts
@@ -10,3 +10,12 @@ export type NewUser = {
 export type User = NewUser & EntityMeta;
 
 export type LoginUser = Omit<NewUser, "first_name" | "last_name">;
+
+export type ApplyUser = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone_number: number;
+  has_drivers_license: boolean;
+  user_id: number;
+};
