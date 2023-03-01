398fb7d472d7a1568829e2313258d94cfc129544
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-02 00:10:41 +0200

    updated jobs page with date and price selects

--- src/pages/Jobs/Jobs.tsx
+++ src/pages/Jobs/Jobs.tsx
@@ -1,11 +1,21 @@
+import { DateOption, PriceOption } from "../../types/select";
 import { borderRadius, darkGrey, mainBgColor } from "../../const/styles";
+import { dateOptions, priceOptions } from "../../const/selectOptions";
+import {
+  sortSelectByDateAsc,
+  sortSelectByDateDesc,
+  sortSelectByPriceAsc,
+  sortSelectByPriceDesc,
+} from "../../utils/select";
 
 import Button from "../../components/Button/Button";
 import Emoji from "../../components/Emoji/Emoji";
 import JobAdForm from "./JobAdForm";
 import JobApplicationForm from "./JobApplicationForm";
 import JobCard from "./JobCard";
+import Select from "react-select";
 import StyledModal from "../../components/StyledModal/StyledModal";
+import StyledSelect from "../../components/Select/StyledSelect";
 import styled from "styled-components";
 import { useJobs } from "../../hooks/jobsHooks";
 import { useState } from "react";
@@ -14,6 +24,8 @@ const Jobs = () => {
   const { data: jobs, isLoading } = useJobs();
   const [adFormOpen, setAdFormOpen] = useState(false);
   const [applicationFormOpen, setApplicationFormOpen] = useState(false);
+  const [selectedDateOption, setSelectedDateOption] = useState<DateOption>();
+  const [selectedPriceOption, setSelectedPriceOption] = useState<PriceOption>();
 
   const handleToggleAdForm = () => {
     setAdFormOpen((prevOpen) => !prevOpen);
@@ -23,12 +35,40 @@ const Jobs = () => {
     setApplicationFormOpen((prevOpen) => !prevOpen);
   };
 
+  const handleDateSortChange = (selectedOption: any) => {
+    setSelectedDateOption(selectedOption);
+    setSelectedPriceOption(undefined);
+  };
+
+  const handlePriceSortChange = (selectedOption: any) => {
+    setSelectedPriceOption(selectedOption);
+    setSelectedDateOption(undefined);
+  };
+
   if (isLoading) {
     return <div>Jobs are loading...</div>;
   }
 
   if (!isLoading && !jobs?.length) {
     return <div>There are no jobs added yet</div>;
+  }
+
+  let sortedJobs = [...jobs];
+
+  if (selectedDateOption) {
+    if (selectedDateOption.value === "descending") {
+      sortedJobs = sortSelectByDateDesc(sortedJobs);
+    } else if (selectedDateOption.value === "ascending") {
+      sortedJobs = sortSelectByDateAsc(sortedJobs);
+    }
+  }
+
+  if (selectedPriceOption) {
+    if (selectedPriceOption.value === "highest") {
+      sortedJobs = sortSelectByPriceDesc(sortedJobs);
+    } else if (selectedPriceOption.value === "lowest") {
+      sortedJobs = sortSelectByPriceAsc(sortedJobs);
+    }
   }
 
   return (
@@ -42,9 +82,23 @@ const Jobs = () => {
           onClick={handleToggleAdForm}
           title="post a job"
         />
+        <StyledSelect>
+          <Select
+            options={dateOptions}
+            value={selectedDateOption}
+            onChange={handleDateSortChange}
+            placeholder="Sort by starting date"
+          />
+          <Select
+            options={priceOptions}
+            value={selectedPriceOption}
+            onChange={handlePriceSortChange}
+            placeholder="Sort by salary"
+          />
+        </StyledSelect>
       </TopContainer>
       <JobsContainer>
-        {jobs.map((job, index) => (
+        {sortedJobs.map((job, index) => (
           <JobCard
             key={index}
             job={job}
