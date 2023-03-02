63e0b00f7793875f9fe37801f274b267c89c11e9
wantyq <navikas.saidas@gmail.com>
2023-03-02 19:49:30 +0200

    comment test

--- src/types/job.ts
+++ src/types/job.ts
@@ -6,7 +6,7 @@ export type JobType = "freelance" | "partTime" | "fullTime";
 export type NewJob = {
   title: string;
   price: number | "";
-  company_logo: string;
+  company_logo: string; // new comment
   type: JobType;
   starting_from: string;
   has_drivers_license: boolean;
