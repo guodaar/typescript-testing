fa77e8f266e32b94df2609c1cd32e1cd53fdbea6
ingaSiu <ingasiud@gmail.com>
2023-03-02 18:55:11 +0200

    fixed colors

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,4 +1,5 @@
 import { MutatingDots } from 'react-loader-spinner';
+import { darkBlue } from '../../const/styles';
 
 type Props = {
   isLoading: boolean;
@@ -9,8 +10,8 @@ const Loader = ({ isLoading }: Props) => {
     <MutatingDots
       height="100"
       width="100"
-      color="rgb(18,0,108)"
-      secondaryColor="rgb(18,0,108)"
+      color={`${darkBlue}`}
+      secondaryColor={`${darkBlue}`}
       radius="12.5"
       ariaLabel="mutating-dots-loading"
       wrapperStyle={{ justifyContent: 'center', height: '100vh', alignItems: 'center' }}
