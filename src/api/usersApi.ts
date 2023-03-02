9c09a04e7d8469d23e835e7c9dbccbc4177e2bfa
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-03-02 21:23:11 +0200

    changed api url

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
 import { LoginUser, NewUser, User } from "../types/user";
 
-const USERS_API_URL = "https://testapi.io/api/rokasandreikenas/resource/user";
+import axios from "axios";
+
+const USERS_API_URL = "https://testapi.io/api/guodaarr/resource/usersServer";
 
 export const fetchUsers = async (): Promise<User[]> => {
   const response = await axios.get(USERS_API_URL);
@@ -17,8 +18,7 @@ export const loginUser = async (loggingUser: LoginUser): Promise<User> => {
   const users = await fetchUsers();
   return new Promise((resolve, reject) => {
     const { email, password } = loggingUser;
-    const userChecker = (u: User) =>
-      u.email === email && u.password === password;
+    const userChecker = (u: User) => u.email === email && u.password === password;
     const existingUser = users.find(userChecker);
 
     existingUser ? resolve(existingUser) : reject("Invalid credentials");
