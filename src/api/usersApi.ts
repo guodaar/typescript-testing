8358af3b8f2ba57dd4981388bdc7613de8f96797
Mikalojus <mikalojus.zirgulis@gmail..com>
2023-03-05 15:15:28 +0200

    added delete-user api request

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
 import { LoginUser, NewUser, User } from "../types/user";
 
-const USERS_API_URL = "https://testapi.io/api/rokasandreikenas/resource/user";
+import axios from "axios";
+
+const USERS_API_URL = "https://testapi.io/api/guodaarr/resource/usersServer";
 
 export const fetchUsers = async (): Promise<User[]> => {
   const response = await axios.get(USERS_API_URL);
@@ -10,6 +11,11 @@ export const fetchUsers = async (): Promise<User[]> => {
 
 export const createUser = async (newUser: NewUser): Promise<User> => {
   const response = await axios.post(USERS_API_URL, newUser);
+  return response.data;
+};
+
+export const deleteUser = async (id: number): Promise<User> => {
+  const response = await axios.delete(`${USERS_API_URL}/${id}`);
   return response.data;
 };
 
