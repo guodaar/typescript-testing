1843ac58694f4229cb07cc47f0f948232e6615bc
wantyq <navikas.saidas@gmail.com>
2023-03-06 21:42:29 +0200

    Layout to routes added

--- src/routes/const.ts
+++ src/routes/const.ts
@@ -1,11 +1,14 @@
 import Jobs from "../pages/Jobs/Jobs";
+import MainLayout from "../layouts/MainLayout";
 import Users from "../pages/Users/Users";
-import { Route } from "../types/routes";
 
 export const JOBS_PATH = "/";
 export const USERS_PATH = "/users";
 
-export const routes: Route[] = [
-  { path: JOBS_PATH, Component: Jobs },
-  { path: USERS_PATH, Component: Users },
-];
+export const mainLayoutRoutes = {
+  Layout: MainLayout,
+  routes: [
+    { path: JOBS_PATH, Component: Jobs },
+    { path: USERS_PATH, Component: Users },
+  ],
+};
