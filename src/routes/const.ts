7d165c10117b65805c215524499c2c3c44817762
Lukasnvc <lukasnvc@gmail.com>
2023-03-07 20:07:03 +0200

    privacy page

--- src/routes/const.ts
+++ src/routes/const.ts
@@ -1,14 +1,17 @@
 import Jobs from "../pages/Jobs/Jobs";
 import MainLayout from "../layouts/MainLayout";
+import Privacy from "../pages/Privacy/Privacy";
 import Users from "../pages/Users/Users";
 
 export const JOBS_PATH = "/";
 export const USERS_PATH = "/users";
+export const PRIVACY_PATH = "/privacy"
 
 export const mainLayoutRoutes = {
   Layout: MainLayout,
   routes: [
     { path: JOBS_PATH, Component: Jobs },
     { path: USERS_PATH, Component: Users },
+    { path: PRIVACY_PATH, Component: Privacy}
   ],
 };
