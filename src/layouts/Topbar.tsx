3d2acf7e6483f59ffd5712b3cb2939ab72d723dc
wantyq <navikas.saidas@gmail.com>
2023-03-08 18:51:08 +0200

    User area created

--- src/layouts/Topbar.tsx
+++ src/layouts/Topbar.tsx
@@ -1,18 +1,22 @@
 import { JOBS_PATH, USERS_PATH } from "../routes/const";
+import { useContext, useState } from "react";
 
 import Button from "../components/Button/Button";
+import { FaAngleDown } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import LoginForm from "../pages/Jobs/LoginForm";
 import RegisterForm from "../pages/Register/RegisterForm";
 import StyledModal from "../components/StyledModal/StyledModal";
+import { UserContext } from "../contexts/UserContext";
 import styled from "styled-components";
-import { useState } from "react";
 
 const Topbar = () => {
   const [loginFormOpen, setLoginFormOpen] = useState(false);
   const [registerOpen, setRegisterOpen] = useState(false);
 
-  
+  const { user, isLoggedIn, handleLogOut } = useContext(UserContext);
+
+
   const handleToggleLoginForm = () => {
     setLoginFormOpen((prevOpen) => !prevOpen);
   };
@@ -34,12 +38,30 @@ const Topbar = () => {
           <StyledLink to={USERS_PATH}>Users</StyledLink>
         </MenuContainer>
         <UserArea>
-          <Button onClick={handleToggleLoginForm} title="log In" greyVariant />
-          <Button
-            onClick={handleToggleRegisterForm}
-            title="Register"
-            greyVariant
-          />
+          <UserMenuFlayout>Flayout menu content</UserMenuFlayout>
+          {isLoggedIn ? (
+            <>
+              <Avatar
+                src={process.env.PUBLIC_URL + "/avatar.jpg"}
+                alt="avatar"
+              />
+              <UserName>{user?.first_name}</UserName>
+              <StyledArrow />
+            </>
+          ) : (
+            <>
+              <Button
+                onClick={handleToggleLoginForm}
+                title="log In"
+                greyVariant
+              />
+              <Button
+                onClick={handleToggleRegisterForm}
+                title="Register"
+                greyVariant
+              />
+            </>
+          )}
         </UserArea>
       </RightSide>
       <StyledModal
@@ -82,10 +104,49 @@ const MenuContainer = styled.div`
   height: 50px;
 `;
 
+const UserMenuFlayout = styled.div`
+  position: absolute;
+  top: 70px;
+  right: 10px;
+  width: 200px;
+  background-color: gray;
+  border-radius: 4px;
+  padding: 8px;
+  display: none;
+  color: white;
+  justify-content: center;
+  align-items: center;
+`;
+
 const UserArea = styled.div`
   display: flex;
   flex-direction: row;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover ${UserMenuFlayout} {
+    display: flex;
+  }
 `;
+
+const Avatar = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+
+const UserName = styled.p`
+  font-weight: 500;
+  font-size: 1.2rem;
+  color: #2b2a2a;
+`;
+
+const StyledArrow = styled(FaAngleDown)`
+  font-size: 1.2rem;
+  color: #2b2a2a;
+`;
+
 
 const StyledLink = styled(Link)`
   color: #261ea7;
