a6de8c0789007a3a0e4945bd1f2d5ade8178405f
Niekada <erik.tankeliun@gmail.com>
2023-03-08 19:58:39 +0200

    mergedd

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,34 +1,48 @@
 import { useState } from "react";
 import styled from "styled-components";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import { mainBgColor } from "../../const/styles";
 import { useUsers } from "../../hooks/userHooks";
 import UserCard from "../../pages/Users/UserCard";
+import { borderRadius, darkGrey, mainBgColor } from "../../const/styles";
+import { motion } from "framer-motion";
+
 
 const Users = () => {
     const { data } = useUsers();
     const users = data || [];
     const [search, setSearch] = useState("");
   
-    const filteredBySearch = users.filter((user) => user.email.toLowerCase().includes(search) || user.first_name.toLowerCase().includes(search))
+    const filteredBySearch = users.filter((user) => user.email.toLowerCase().includes(search) || user.first_name.toLowerCase().includes(search));
 
   return (
-    <Container>
-      <SearchBar 
-        value={search}
-        setValue={setSearch}
-      />
-      {filteredBySearch.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
-    </Container>
+    <motion.div
+        animate={{opacity: 1}}
+        initial={{opacity: 0}}
+        exit={{opacity: 0}}
+        transition={{duration: 0.5}}
+    >
+        <Container>
+        <SearchBar 
+            value={search}
+            setValue={setSearch}
+        />
+        {filteredBySearch.map((user) => (
+            <UserCard key={user.id} user={user} />
+        ))}
+        </Container>
+    </motion.div>
   )
 }
 
 export default Users;
 
 const Container = styled.div`
-  max-width: 1100px;
-  margin: 40px auto;
-  background-color: ${mainBgColor};
+ background-color: ${mainBgColor};
+  margin: 50px 15vw;
+  padding: 32px;
+  border-radius: ${borderRadius};
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  color: ${darkGrey};
 `;
