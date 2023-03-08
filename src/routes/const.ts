6189b8f31de2f9fed434d7597c039a77486990fd
Tomas Krinickas <tomas.krinickas2@gmail.com>
2023-03-08 21:41:42 +0200

    added route to job ad page

--- src/routes/const.ts
+++ src/routes/const.ts
@@ -1,3 +1,4 @@
+import JobAdPage from "../pages/JobAdPage/JobAdPage";
 import Jobs from "../pages/Jobs/Jobs";
 import MainLayout from "../layouts/MainLayout";
 import Privacy from "../pages/Privacy/Privacy";
@@ -6,6 +7,7 @@ import Users from "../pages/Users/Users";
 export const JOBS_PATH = "/";
 export const USERS_PATH = "/users";
 export const PRIVACY_PATH = "/privacy";
+export const JOB_AD_PATH = "/:id";
 
 export const mainLayoutRoutes = {
   Layout: MainLayout,
@@ -13,5 +15,6 @@ export const mainLayoutRoutes = {
     { path: JOBS_PATH, Component: Jobs, title: "Jobs" },
     { path: USERS_PATH, Component: Users, title: "Users" },
     { path: PRIVACY_PATH, Component: Privacy, title: "Privacy" },
+    { path: JOB_AD_PATH, Component: JobAdPage, title: "" },
   ],
 };
